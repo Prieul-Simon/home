f953cd72ec8400ab3224ea578e382d05789cd68f
Simon Prieul <simon.prieul@gmail.com>
2026-02-06 17:12:14 +0100

    Little refactor o setup-completions

--- scripts/bun/setup-completions.ts
+++ scripts/bun/setup-completions.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env bun
 
 import { $, write } from "bun"
-import { retrieveRawContent } from "./src/common/common"
+import { retrieveRawContent as commonRetrieveRawContent } from "./src/common/common"
+import { exists } from "fs/promises"
 
 async function setupAll() {
     await Promise.allSettled([
@@ -13,40 +14,91 @@ async function setupAll() {
     ])
 }
 
-async function setupTmux() {
+await setupAll()
+process.exit(0)
+
+type Category = 
+    | 'tmux'
+    | 'bun'
+    | 'npm'
+    | 'node'
+    | 'tldr'
+
+function syncLog(category: Category, msg: string, ...data: any[]) {
+    const beginCategory = `[${category}]`
+    const endCategory = `[${category}]`
+    console.info(beginCategory)
+    console.info(`    ${msg}`, ...data) // indent 4 spaces
+    console.info(endCategory)
+}
+
+async function testFileExists(category: Category, filePath: string): Promise<boolean> {
+    if (await exists(filePath)) {
+        syncLog(category, "%s already exists, the script won't try to re-fetch it", filePath)
+        return true
+    }
+    return false
+}
+function logSuccess(category: Category, filePath: string) {
+    syncLog(category, "%s created !", filePath)
+}
+
+async function retrieveRawContent(category: Category, url: string): Promise<string> {
+    return await commonRetrieveRawContent(
+        url,
+        (msg: string) => syncLog(category, msg)
+    )
+}
+
+async function setupTmux(category: Category = 'tmux') {
     const relativeLocation = `../bashrc/tmux`
+    const completionFile = `${relativeLocation}/tmux.completion.bash`
+    if (await testFileExists(category, completionFile)) return
+
     // const completionUrl = `https://raw.githubusercontent.com/Bash-it/bash-it/refs/heads/master/completion/available/tmux.completion.bash`
     const completionUrl = `https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux`
 
     const [completionResult] = await Promise.allSettled<string>([
-        retrieveRawContent(completionUrl),
+        retrieveRawContent(category, completionUrl),
     ])
 
     // Create real files
-    const completionFile = `${relativeLocation}/tmux.completion.bash`
     if (completionResult?.status === 'fulfilled') {
         await write(completionFile, completionResult.value)
+        logSuccess(category, completionFile)
     }
-    console.info('setup-tmux-completions finished !')
 }
 
-async function setupBun() {
-    await $`bun completions > ../bashrc/sh/bun.completion.bash`
+async function setupBun(category: Category = 'bun') {
+    const completionFile = '../bashrc/sh/bun.completion.bash'
+    if (await testFileExists(category, completionFile)) return
+
+    await $`bun completions > ${completionFile}`
+    logSuccess(category, completionFile)
 }
 
-async function setupNpm() {
-    await $`npm completion > ../bashrc/sh/npm.completion.sh`
+async function setupNpm(category: Category = 'npm') {
+    const completionFile = '../bashrc/sh/npm.completion.sh'
+    if (await testFileExists(category, completionFile)) return
+
+    await $`npm completion > ${completionFile}`
+    logSuccess(category, completionFile)
 }
 
-async function setupNode() {
-    await $`node --completion-bash > ../bashrc/sh/node.completion.bash`
+async function setupNode(category: Category = 'node') {
+    const completionFile = '../bashrc/sh/node.completion.bash'
+    if (await testFileExists(category, completionFile)) return
+
+    await $`node --completion-bash > ${completionFile}`
+    logSuccess(category, completionFile)
 }
 
-async function setupTldr() {
+async function setupTldr(category: Category = 'tldr') {
+    const completionFile = '../bashrc/sh/tldr.completion.bash'
+    if (await testFileExists(category, completionFile)) return
+
     const completionUrl = `https://raw.githubusercontent.com/tldr-pages/tldr-node-client/refs/heads/main/bin/completion/bash/tldr`
-    const completionResult = await retrieveRawContent(completionUrl)
-    await write('../bashrc/sh/tldr.completion.bash', completionResult)
+    const completionResult = await retrieveRawContent(category, completionUrl)
+    await write(completionFile, completionResult)
+    logSuccess(category, completionFile)
 }
-
-await setupAll()
-process.exit(0)
--- scripts/bun/src/common/common.ts
+++ scripts/bun/src/common/common.ts
@@ -1,21 +1,45 @@
 import type { PathLike } from "fs"
 import { exists, symlink } from "fs/promises"
 
-export async function retrieveRawContent(url: string): Promise<string> {
-    console.info(`Fetching '${url}'...`)
+export async function retrieveRawContent(
+    url: string, 
+    logFn?: typeof console.info,
+    errorFn?: typeof console.error,
+): Promise<string> {
+    log(`Fetching '${url}'...`, logFn)
     const rawContent = await fetch(url)
     if (rawContent.status !== 200) {
-        console.error('Not the expected result from GitHub: %o', rawContent)
+        error('Not the expected result from GitHub: %o', rawContent, errorFn)
         process.exit(1)
     }
-    console.info(`End fetching '${url}' with success`)
+    log(`End fetching '${url}' with success`, logFn)
     return rawContent.text()
 }
 
-export async function symlinkIfUnexisting(target: PathLike, nameLinkToCreate: PathLike) {
+export async function symlinkIfUnexisting(
+    target: PathLike, 
+    nameLinkToCreate: PathLike,
+    logFn?: typeof console.info,
+) {
     if (await exists(nameLinkToCreate)) {
-        console.info(`Will not create '${nameLinkToCreate}' as it already exists`)
+        log(`Will not create '${nameLinkToCreate}' as it already exists`, logFn)
         return
     }
     await symlink(target, nameLinkToCreate)
+}
+
+function log(msg: string, logFn?: typeof console.info) {
+    if (logFn) {
+        logFn(msg)
+    } else {
+        console.info(msg)
+    }
+}
+
+function error(msg: string, data: any, errorFn?: typeof console.error) {
+    if (errorFn) {
+        errorFn(msg, data)
+    } else {
+        console.error(msg, data)
+    }
 }
