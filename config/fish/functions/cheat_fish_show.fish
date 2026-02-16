a9364ae25ec2178f255c36382ba8a4ce9f6c2ed3
Simon Prieul <simon.prieul@gmail.com>
2026-02-16 21:36:27 +0100

    Add fish aliases

--- config/fish/functions/cheat_fish_show.fish
+++ config/fish/functions/cheat_fish_show.fish
@@ -2,5 +2,7 @@ function cheat_fish_show --description 'Print help for fish shell'
     echo "\
 > Ctrl-A        Move cursor to the beginning of the line.
 > Ctrl-E        Move cursor to the end of the line.
-> Ctrl-U        Delete from cursor to the beginning of the line."
+> Ctrl-U        Delete from cursor to the beginning of the line.
+> Ctrl-R        (fzf) Complet current command through history
+> Ctrl-T        (fzf) Complete current command"
 end
