db8efc53eb73bac822997f09ebc4b4617ef4ba7d
Simon Prieul <simon.prieul@gmail.com>
2026-02-24 14:48:43 +0100

    greeting message in bash shell

--- scripts/bashrc/importme.bashrc.bash
+++ scripts/bashrc/importme.bashrc.bash
@@ -11,6 +11,9 @@
 # ```
 #####################################################################################################################################################
 
+# Welcome message
+echo "You are currently in bash shell"
+
 ## XDG spec
 ## https://wiki.archlinux.org/title/XDG_Base_Directory
 export XDG_CONFIG_HOME="$HOME/.config"
