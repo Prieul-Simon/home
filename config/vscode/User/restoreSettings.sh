aef4a3bb1824ad5655a9cb77d300d0dae74852f6
Simon Prieul <simon.prieul@gmail.com>
2026-02-27 15:13:33 +0100

    Ad bugfixer for VSCode

---
+++ config/vscode/User/restoreSettings.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Fix for that f*chink annoying bug from VSCode
+# https://github.com/microsoft/vscode/issues/297887
+cp ~/utils/config/vscode/User/settings.bak.json ~/utils/config/vscode/User/settings.json
